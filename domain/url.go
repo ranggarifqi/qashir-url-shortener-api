8e2578481cd58b03dabb68305c752627ec8323e5
Rangga Rifqi Pratama <rangga@ranggarifqi.com>
2021-06-10 22:54:29 +0700

    URL Usecase

--- domain/url.go
+++ domain/url.go
@@ -1,11 +1,21 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	"github.com/ranggarifqi/qashir-api/helper"
+	"gorm.io/gorm"
+)
 
 type Url struct {
 	ID           string    `json:"id"`
 	ShortenedUrl string    `json:"url" validate:"required" gorm:"not null"`
 	CreatedAt    time.Time `json:"created_at"`
+}
+
+func (u *Url) BeforeCreate(tx *gorm.DB) (err error) {
+	u.ID = helper.GenerateID(6)
+	return nil
 }
 
 type CreateUrlDto struct {
