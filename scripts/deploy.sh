69aea44b64ee9edb3a58725f874d831828688a85
Rangga Rifqi Pratama <rangga@ranggarifqi.com>
2021-06-15 21:47:07 +0700

    Fix deploy.sh again

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 docker build -t ranggarifqi/qashir-backend .
 curl https://cli-assets.heroku.com/install.sh | sh
 heroku auth:token
-heroku container:login
-heroku container:push web -a qashir/backend
-heroku container:release web -a qashir/backend
+# heroku container:login
+docker login --username=_ --password=$(heroku auth:token) registry.heroku.com
+docker tag ranggarifqi/qashir-backend registry.heroku.com/qashir-backend/web
+docker push registry.heroku.com/qashir-backend/web
+heroku container:release web
